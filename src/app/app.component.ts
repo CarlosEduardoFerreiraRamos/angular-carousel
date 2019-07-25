8c07e0bdc0d6cb9874c24f6509298607920a6a94
Carlos Ramos <carlos.ramos@decoracontent.com>
2019-07-25 11:35:55 -0300

    IT WORKS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+// import * as jQuery from 'jquery';
+declare var $: any;
+declare var jQuery: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-carousel';
+  @ViewChild('jcarousel') j: any;
+
+  ngAfterViewInit() {
+    $(document).ready(() => {
+      console.log('READY')
+    });
+    $('h1').click( () => console.log('click'))
+
+    // console.log(this.j.jcarousel)
+    // console.log($('.jcarousel'))
+    $('.jcarousel').jcarousel({
+    //     // Configuration goes here
+    });
+  }
+
+  onMoveRight() {
+    $('.jcarousel').jcarousel('scroll', '+=1');
+  }
+
+  onMoveLeft() {
+    $('.jcarousel').jcarousel('scroll', '-=1');
+  }
 }
